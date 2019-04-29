73bb0d07c207328c616304b51b46c5cf1eb45a8d
Audi-ana Brown <audi-anabrown@Audi-anas-MacBook-Pro.local>
2019-04-29 15:21:24 -0500

    Getting used to the flow of redux

---
+++ index.js
@@ -0,0 +1,45 @@
+const redux = require('redux')
+
+const initialState = {
+    counter: 0
+}
+
+//create a reducer -> actually executes the update/action
+let rootReducer = (state = initialState, action) =>{
+    if(action.type == "DEC_COUNTER"){
+        return{
+            ...state, 
+            counter: state.counter - 1
+        }
+    }
+    else if(action.type == "SUB_COUNTER") {
+    return {
+        ...state,
+        counter: state.counter + action.value
+        }
+    }
+    return state 
+}
+
+// //create a store
+const store = redux.createStore(rootReducer)
+
+store.subscribe(() => {
+    console.log("[Subscribed]",store.getState())
+  })
+
+console.log(store.getState())
+//dispatching actions -> triggers a state change
+console.log("Dispatching DEC_COUNTER action")
+
+
+store.dispatch({
+    type:"SUB_COUNTER",
+    value: 10
+})
+
+store.dispatch({
+    type: 'DEC_COUNTER'
+})
+
+console.log(store.getState())
